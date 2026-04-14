96828a008c2927b86dda2d867f8c8820decc9b53
María Reyes <marupaula.2004@gmail.com>
2026-04-14 17:37:04 -0500

    T

--- password_manager.py
+++ password_manager.py
@@ -14,11 +14,31 @@ def encrypt_single_pass(filename : str) -> None:
 if __name__ == "__main__":
     encrypt_single_pass("examples/example1.txt")
 
+#Parte 2 
 
 def encrypt_passwords_in_file(filename: str) -> None:
-    """TODO: Parte 2."""
-    pass
+    with open(filename, "r") as file:
+        reader = csv.reader(file)
+        data = []
+        for file in reader:
+            data.append(row)
+    
+        for index, row in enumerate(data):
+            encrypted_password = caesar_encrypt(row[2])
+            data[index][2] = encrypted_password
+    
+    with open(filename, "w") as file:
+        writer = csv.writer(file)
+        for row in data:
+            encrypted_password = caesar_encrypt(row[2])
+            writer.writerow([row[0], row[1], encrypted_password])  
 
+if __name__ == "__main__":
+    encrypt_passwords_in_file("examples/example2.csv")
+
+
+
+#Parte 3
 
 def change_password(filename: str, website: str, password: str) -> bool:
     """TODO: Parte 3."""
